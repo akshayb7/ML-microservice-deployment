8421f25d8f13583a4d3e3f1b1f7d91cde7371115
akshayb7 <akshay10.bhardwaj@gmail.com>
2020-08-31 02:44:40 +0530

    ADD: Run the dockerfile on local minikube cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=akshayb7/boston_houseprice:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run houseprice --image=akshayb7/boston_houseprice:latest --port=80 --cluster='minikube'
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/houseprice 8000:80
